b88259f9a1b9c8b3457178df6d21af3fcd2a62f7
Tomislav Stojanov <tomislav.stojanov@protonmail.com>
2019-08-07 11:07:29 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Changed on the Feature branch
         </a>
       </header>
     </div>
